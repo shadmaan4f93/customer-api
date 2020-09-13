604e64c938ad25b0238484b4e62e44bdcbcfe0a8
shadmaan <shadmaan4f93@gmail.com>
2020-09-13 21:30:03 +0530

    Setting up home view

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
+from django.http import HttpResponse
+
 from .serializers import CustomerSerializer
 from .models import Customer
+
+
+def index(request):
+    return HttpResponse("<div style='text-align: center;margin-top: 100px;'><h1>Customer Demo Api</h1></div>")
 
 class CustomerViewSet(viewsets.ModelViewSet):
     """ This class is used to perform CRUD operation over customer table"""
--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from customer import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path("api/", include("customer.urls")),
 ]
